c2a89a47ad7cecbe877bd5f35580570c55060d96
ozairkhan2810 <ozairkhan2810@gmail.com>
2022-03-22 22:14:05 +0530

    style: navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./navbar.css";
+import SearchBar from "./SearchBar";
+import logo from "../../assets/crickit-logo.png";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="navbar flex align-center p-1">
+        <div className="header-menu">
+          <span class="material-icons"> menu </span>
+        </div>
+        <Link to="/">
+          <img className="header-logo" src={logo} alt="crickit-logo" />
+        </Link>
+        <SearchBar />
+        <Link to="/login" className="header-link">
+          <div className="header-login-link">
+            Login
+          </div>
+        </Link>
+        <Link to="/wishlist" className="header-link">
+          <div className="header-wishlist-link rel">
+            <span className="material-icons"> favorite_border </span>
+            <span className="badge-icon">5</span>
+          </div>
+        </Link>
+        <Link to="/cart" className="header-link">
+          <div className="header-cart-link rel mr-1">
+            <span className="material-icons"> shopping_cart </span>
+            <span className="badge-icon">5</span>
+          </div>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
