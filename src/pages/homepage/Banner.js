04760a4fba6641ec192b58812ee4db6dd59cbdec
ozairkhan2810 <ozairkhan2810@gmail.com>
2022-03-24 10:35:06 +0530

    style: banner image

--- src/pages/homepage/Banner.js
+++ src/pages/homepage/Banner.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import banner from "../../assets/ecomm-banner.jpg";
+import "./banner.css";
 
 const Banner = () => {
   return (
     <Link to="/products">
-      <div style={{ marginTop: "3.25rem" }} className="banner border-radius rel">
-        <img className="img-banner" src={banner} alt="banner" />
+      <div className="banner border-radius rel">
+        <img
+          className="img-banner"
+          src="./assets/ecomm-banner.jpg"
+          alt="banner"
+        />
         <div className="banner-btn pr-1 pb-1 flex gap-1">
           <button className="btn btn-primary">SHOP NOW</button>
         </div>
