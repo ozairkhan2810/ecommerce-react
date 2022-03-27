8789f62ca77cdb1befe4d82d79755b34d7273bbd
ozairkhan2810 <ozairkhan2810@gmail.com>
2022-03-28 03:45:29 +0530

    refactor: header and footer

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,16 +1,12 @@
 import React from "react";
-import Navbar from "../../components/navbar/Navbar";
 import Banner from "../../components/homepage/Banner";
 import Categories from "../../components/homepage/Categories";
-import Footer from "../../components/footer/Footer";
 
 const Homepage = () => {
   return (
     <div>
-      <Navbar />
       <Banner />
       <Categories />
-      <Footer />
     </div>
   );
 };
