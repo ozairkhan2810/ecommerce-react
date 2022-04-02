0df10288cec83f6fe6d2c4ea4973b3c9d7d0af8d
ozairkhan2810 <ozairkhan2810@gmail.com>
2022-04-02 18:28:36 +0530

    refactor: typo className

--- src/components/homepage/Categories.js
+++ src/components/homepage/Categories.js
@@ -4,21 +4,21 @@ import { categories } from "../../backend/db/categories";
 
 const Categories = () => {
   return (
-    <div class="deal-container p-1 border-radius bg-white box-shadow">
-      <h2 class="center-text pb-1">Categories</h2>
-      <div class="category flex gap-1 mb-1">
+    <div className="deal-container p-1 border-radius bg-white box-shadow">
+      <h2 className="center-text pb-1">Categories</h2>
+      <div className="category flex gap-1 mb-1">
         {categories.map((category, index) => (
           <Link to="/products">
             <div
               key={category._id}
-              class="category-list flex flex-column gap-1 justify-center align-center bg-white box-shadow"
+              className="category-list flex flex-column gap-1 justify-center align-center bg-white box-shadow"
             >
               <img
-                class="img-category"
+                className="img-category"
                 src={category.categoryImg}
                 alt={category.categoryName}
               />
-              <span class="bold-500">{category.categoryName}</span>
+              <span className="bold-500">{category.categoryName}</span>
             </div>
           </Link>
         ))}
